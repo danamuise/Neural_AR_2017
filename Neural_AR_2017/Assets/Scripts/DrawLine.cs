228ef9d2e01dad416a6bbd633e8dddcf606dca4c
Dana Muise <Dana Muise>
2017-02-28 12:06:04 -0800

    first UI

--- Neural_AR_2017/Assets/Scripts/DrawLine.cs
+++ Neural_AR_2017/Assets/Scripts/DrawLine.cs
@@ -1,39 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class DrawLine : MonoBehaviour {
 
+
 	private LineRenderer lineRenderer;
-	private float counter;
-	private float dist;
 	public Transform origin;
 	public Transform destination;
-	public float lineDrawSpeed = 6f;
-	public float line_width = 0.1f;
-	Vector3 newPos = new Vector3 (0, 0, 0);
-	public Canvas canvas;
+	public float line_width = 0.01f;
 
 	// Use this for initialization
 	void Start ()
 	{
 		lineRenderer = GetComponent<LineRenderer> ();
-		lineRenderer.SetPosition(0, origin .position);
 		lineRenderer.SetWidth(line_width, line_width);
-		dist = Vector3.Distance(origin.position, destination.position);
+		lineRenderer.SetColors (Color.white, Color.white);
+		Material whiteDiffuseMat = new Material (Shader.Find ("Unlit/Texture"));
+		lineRenderer.material = whiteDiffuseMat;
 	}
 
 	//Update is called once per framce
 	void Update ()
 	{
-		//Rotate destination object to face camera
-		canvas.transform.rotation = Camera.main.transform.rotation;
+		//Rotate this object to face camera
+		transform.rotation = Camera.main.transform.rotation;
 
-		newPos.x = destination.position.x - destination.GetComponent<RectTransform> ().rect.width/2;
-		newPos.y =  destination.position.y - destination.GetComponent<RectTransform> ().rect.height/2;
-		newPos.z = destination.position.z;
-	
-		//Debug.Log ("Dest.rot: " + destination.rotation.ToString());
-
+		//draw line
 		lineRenderer.SetPosition(0, origin.position);
 		lineRenderer.SetPosition(1, destination.position);
 	}
